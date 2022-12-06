f64fd5185c3f3665115d129ae5744a3aa3f555aa
pwwang <pwwang@pwwang.com>
2022-12-06 13:52:26 -0700

    🚨 Format example.py

--- example.py
+++ example.py
@@ -2,18 +2,20 @@
 
 from pipen import Proc, Pipen
 
+
 class P1(Proc):
     input = "infile:file"
     output = "outfile:file:out.txt"
     script = "sleep 30; echo whatever > {{out.outfile}}"
+
 
 class P2(P1):
     requires = P1
     # If we have typo, dry-running can detect it real quick
     # input_data = lambda ch: [ch.iloc[0,0] + "1"]
 
+
 if __name__ == "__main__":
-    Pipen(
-        loglevel="debug",
-        scheduler="dry"
-    ).set_start(P1).set_data([__file__]).run()
+    Pipen(loglevel="debug", scheduler="dry").set_start(P1).set_data(
+        [__file__]
+    ).run()
